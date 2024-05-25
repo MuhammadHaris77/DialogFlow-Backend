7db574c065b68e1314f1aa49e84b85e5350c3864
muhammad <harisqureshi415@gmail>
2024-05-25 21:03:12 +0500

    email send from backend

--- dialogflow.js
+++ dialogflow.js
@@ -2,6 +2,7 @@ const dialogflow = require('@google-cloud/dialogflow');
 const { WebhookClient, Suggestion, Card } = require('dialogflow-fulfillment');
 const express = require("express")
 const cors = require("cors");
+const  nodemailer = require('nodemailer');
 
 const app = express();
 app.use(express.json())
@@ -15,6 +16,8 @@ app.post("/webhook", async (req, res) => {
     console.log(id)
     const agent = new WebhookClient({ request: req, response: res });
 
+    
+
     function hi(agent) {
         console.log(`intent  =>  hi`);
         agent.add(`Hi i am pizza bot Ai Assistant   how can help you ?? ` )
@@ -24,14 +27,48 @@ app.post("/webhook", async (req, res) => {
         agent.add("fallback from server side?")
     }
 
+
+    
     function order(agent) {
         console.log(`intent  =>  hi`);
-        agent.add("ok sir your is done")
+        const {givenname, phone , address , email} = agent.parameters;
+
+      
+
+    
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'harisqureshi415@gmail.com',
+              pass: 'hfnodldnpfuaagmo'
+            }
+          });
+          
+          var mailOptions = {
+            from: 'harisqureshi415@gmail.com',
+            to: email,
+            subject: 'Sending Email from Backend',
+            text: `Hello Sir/Madam! hope you are received email kindly confirm  your name ${givenname} your contact number ${phone}  and your email ${email} and your addres ${address}  thank you` 
+          };
+        
+          transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+        
+          agent.add("ok sir your is done")
+
+
+
+
     }
 
     let intentMap = new Map();
     intentMap.set('Default Fallback Intent', fallBack); 
-    intentMap.set('Welcome', hi); 
+    intentMap.set('hi', hi); 
     intentMap.set('order', order); 
   
     agent.handleRequest(intentMap);
